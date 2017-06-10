abb1ff52f47f364ac88ec5517f2df86ad860aad8
tnhn <tunahan_007@yahoo.com>
2017-06-10 23:30:04 +0300

    test 1

--- Yeni klasör/test.php
+++ Yeni klasör/test.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tnhn
- * Date: 10.06.2017
- * Time: 23:28
- */
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>deneme  tunaan</title>
+</head>
+<body>
+
+</body>
+</html>
